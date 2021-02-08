74d1a326c4d7de2642339e6e6fe5010650887a87
Yan Linn Thor <yanlinnthor@evolxit.com>
2021-02-08 16:32:49 +0630

    added Slug

--- app/Modules/Post/Models/Post.php
+++ app/Modules/Post/Models/Post.php
@@ -4,14 +4,17 @@ namespace App\Modules\Post\Models;
 
 use App\Modules\Post\Factories\PostFactory;
 use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Modules\Post\Contracts\Post as PostContract;
 use Konekt\Enum\Eloquent\CastsEnums;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Post extends Model implements PostContract
 {
-    use CastsEnums, Uuids, HasFactory;
+    use CastsEnums, Uuids, HasFactory, HasSlug;
 
     protected $table = 'posts';
 
@@ -21,14 +24,33 @@ class Post extends Model implements PostContract
         'type' => 'PostTypeProxy@enumClass'
     ];
 
-    // https://github.com/nWidart/laravel-modules/issues/1094
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 
     /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+
+    /**
+     * // https://github.com/nWidart/laravel-modules/issues/1094
      * Create a new factory instance for the model.
      *
      * @return \Illuminate\Database\Eloquent\Factories\Factory
      */
-    protected static function newFactory()
+    protected static function newFactory(): Factory
     {
         return PostFactory::new();
     }
