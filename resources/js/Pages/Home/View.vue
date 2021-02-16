8d4caeceb08804bc9fddff305127eda55680cd62
Yan Linn Thor <yanlinnthor@evolxit.com>
2021-02-16 18:59:33 +0630

    Changed: single post view layout

--- resources/js/Pages/Home/View.vue
+++ resources/js/Pages/Home/View.vue
@@ -3,57 +3,57 @@
 
     <div class="uk-section">
       <div class="uk-container">
-        <div class="uk-flex-center uk-flex-middle uk-grid-small" uk-grid>
-          <article class="uk-article uk-width-1-1@s">
-            <h3 class="uk-article-title">
-              {{ translate('post.types.' + post.type) }}
-              <small>
-                  <span v-if="post.is_offer" class="uk-label uk-label-success">{{
-                      translate('main.is_offer')
-                    }}</span>
-                <span v-else class="uk-label uk-label-warning">{{ translate('main.not_offer') }}</span>
-              </small>
-            </h3>
-            <hr class="uk-divider-small">
+
+        <h3 class="uk-article-title">
+          {{ translate('post.types.' + post.type) }}
+          <small>
+            <span v-if="post.is_offer" class="uk-label uk-label-success">
+              {{ translate('main.is_offer') }}
+            </span>
+            <span v-else class="uk-label uk-label-warning">{{ translate('main.not_offer') }}</span>
+          </small>
+        </h3>
+
+        <hr class="uk-divider-small">
+
+        <div class="uk-grid-large" uk-grid>
+
+          <article class="uk-article uk-width-2-3@m uk-width-1-1@s">
             <h3>
               {{ post.title }}
             </h3>
             <p class="uk-text-meta">{{ post.duration }} @{{ post.location }}</p>
             <p>{{ post.body }}</p>
-            <div class="uk-grid-small" uk-grid>
-              <!--              <div>-->
-              <!--                <a class="uk-button uk-button-text" href="#">Edit</a>-->
-              <!--              </div>-->
-              <div>
-                <a class="uk-button uk-button-text" href="#">5 Comments</a>
-              </div>
+
+            <div class="uk-margin">
+              <hr class="">
+              <p class="uk-text-meta uk-text-small">{{ translate('comment.view.text') }}</p>
+              <form class="uk-grid-small" uk-grid>
+                <div class="uk-width-1-2@s">
+                  <input :placeholder="translate('comment.view.placeholder')" class="uk-input" type="text">
+                </div>
+                <div class="uk-width-1-2@s">
+                  <button class="uk-button uk-button-secondary" type="submit">
+                    {{ translate('comment.view.submit', {count: 5}) }}
+                  </button>
+                </div>
+              </form>
             </div>
           </article>
-          <div class="uk-width-2-3@s">
+
+          <div class="uk-width-1-3@m uk-width-1-1@s">
+            <h3 class="uk-heading-bullet">{{ translate('comment.new.text') }}</h3>
             <form class="uk-grid-small" uk-grid>
               <div class="uk-width-1-1@s">
-                <textarea id="" class="uk-textarea" name="" placeholder="Enter comment" rows="3"></textarea>
+                <textarea id="" :placeholder="translate('comment.new.placeholder')" class="uk-textarea" name=""
+                          rows="5"></textarea>
               </div>
-              <div class="uk-width-2-3@m uk-width-1-1@s">
-                <input class="uk-input" placeholder="Contact info" type="text">
-              </div>
-              <div class="uk-width-1-3@m uk-width-1-1@s">
-                <button class="uk-button uk-width-1-1">Comment</button>
+              <div class="uk-width-1-1@s">
+                <button class="uk-button uk-align-right">{{ translate('comment.new.submit') }}</button>
               </div>
             </form>
-            <dl class="uk-description-list uk-description-list-divider uk-margin-medium-top">
-              <dt>Description term</dt>
-              <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</dd>
-              <dt>Description term</dt>
-              <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-              </dd>
-              <dt>Description term</dt>
-              <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-              </dd>
-            </dl>
           </div>
+
         </div>
       </div>
     </div>
---
+++ resources/lang/en/comment.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    "view" => [
+        "text" => "Only original poster (OP) can view the comments. OP can enter the Secret Token for this post at below and view the comments.",
+        "placeholder" => "Enter secret token",
+        "submit" => "View :count comments",
+    ],
+    "new" => [
+        "text" => "Leave a comment",
+        "placeholder" => "Enter comment",
+        "submit" => "Submit"
+    ]
+];
