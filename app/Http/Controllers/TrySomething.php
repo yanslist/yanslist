27c85dc66bc104b13b783a471c7a2298d79f1a92
Yan Linn Thor <yanlinnthor@evolxit.com>
2021-02-26 20:50:15 +0630

    Added: installed sendinblue package and setup

--- app/Http/Controllers/TrySomething.php
+++ app/Http/Controllers/TrySomething.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class TrySomething extends Controller
 {
@@ -11,15 +10,10 @@ class TrySomething extends Controller
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response|\Inertia\Response|\Inertia\ResponseFactory
+     * @return int
      */
     public function __invoke(Request $request)
     {
-        $endpoint = config('services.polr.domain').'/api/v2/action/shorten';
-        $response = Http::post($endpoint, [
-            'key' => config('services.polr.key'),
-            'url' => 'https://facebook.com',
-        ]);
-        dd($response->body());
+        return 1;
     }
 }
